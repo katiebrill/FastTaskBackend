56a0cd5b4a8d70573747e948729ea4be8a1b5636
ryankell05 <ryankell@umich.edu>
2017-04-07 17:11:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,7 @@ app.register_blueprint(controllers.pull, url_prefix='/67tgjjckt3trjdhx5hfr/p2')
 
 #set secret key
 app.secret_key = '\x11\x9c\n;[\xa0V\xc3O\xb9\xa1\x0e]fR+\x02\x02\xa8\xacL\x8bq\xcb'
-#
- Listen on external IPs
+#Listen on external IPs
 # For us, listen to port 3000 so you can just run 'python app.py' to start the server
 if __name__ == '__main__':
 	 # listen on external IPs
