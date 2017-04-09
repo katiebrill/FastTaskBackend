37e4b8dddc62fef6b1149ebb840016633d66712b
Aaron Pollack <pollacka@umich.edu>
2017-04-09 15:54:56 -0400

    mm

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app = Flask(__name__, template_folder='templates')
 # Register the controllers
 
 # Url prefix is Heroku Secret Key
-app.register_blueprint(controllers.push, url_prefix='/67tgjjckt3trjdhx5hfr/p2')
-app.register_blueprint(controllers.pull, url_prefix='/67tgjjckt3trjdhx5hfr/p2')
+app.register_blueprint(controllers.push, url_prefix='')
+app.register_blueprint(controllers.pull, url_prefix='')
 
 #set secret key
 app.secret_key = '\x11\x9c\n;[\xa0V\xc3O\xb9\xa1\x0e]fR+\x02\x02\xa8\xacL\x8bq\xcb'
