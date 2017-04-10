a3f88c1e21537b640d5b102ab9e85732b570c41e
Aaron Pollack <pollacka@umich.edu>
2017-04-09 22:49:11 -0400

    updated db credentials

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
 # Credentials should work for group server
 
+#env = dict(
+#	host = 'ec2-54-227-237-223.compute-1.amazonaws.com',
+#	port = 5432,
+#	user = 'root',
+#	password = '980dfe2f08c91592f528492bbc65c80b1a9642197bf2b2b2e985b4659c826b38',
+#	db = 'db62ofjn9t5ctd'
+#)
+
 env = dict(
-	host = 'ec2-54-227-237-223.compute-1.amazonaws.com',
+	host = 'us-cdbr-iron-east-03.cleardb.net',
 	port = 5432,
-	user = 'root',
-	password = '980dfe2f08c91592f528492bbc65c80b1a9642197bf2b2b2e985b4659c826b38',
-	db = 'db62ofjn9t5ctd'
+	user = 'b6d189bfed5cbf',
+	password = '1a4a869d ',
+	db = 'b4d092e21491b8e'
 )
